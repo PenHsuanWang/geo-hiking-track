93ffde9d2aa6022919459f617e2714f02f2eec18
PenHsuanWang <willy118116@gmail.com>
2023-08-30 22:36:21 +0800

    ci setting

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gpx-analyzer',
+    version='0.1',
+    description='A cli tool to extract gpx file and generate map',
+    author='PenHsuanWang',
+    license='MIT',
+    platforms=['unix', 'linux', 'osx', 'win32'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    packages=find_packages(),  # Automatically discover and include all packages
+    install_requires=[
+        'click',
+    ],
+    python_requires='>=3.10',
+    zip_safe=False,
+    extras_require={
+        'testing': [
+            'pytest>=6.0',
+            'pytest-cov>=2.0',
+            'mypy>=0.910',
+            'flake8>3.9',
+            'tox>=3.24',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'gpxana=cli:main',
+        ]
+    }
+)
