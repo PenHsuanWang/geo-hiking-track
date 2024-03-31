c68d3f7112b8508dc48293034bde2c273600dcaa
PenHsuanWang <willy118116@gmail.com>
2024-03-31 15:44:34 +0800

    simply fix the formatter warning raise from mypy

--- src/geo_objects/geo_tracks/basic_track.py
+++ src/geo_objects/geo_tracks/basic_track.py
@@ -1,16 +1,35 @@
-
+from typing import Union
 from src.geo_objects.geo_points.basic_point import BasicPoint
+from src.geo_objects.geo_points.analyzed_geo_points import AnalyzedTrkPoint
+from src.geo_objects.geo_points.raw_geo_points import RawTrkPoint
 
 
 class BasicTracks:
+    """
+    The BasicTracks class serves as a base class for other track classes.
+    It provides basic functionality for managing a list of track points.
+
+    :ivar _main_track_points_list: List of track points
+    :vartype _main_track_points_list: list
+    """
 
     def __init__(self):
         """
-
+        Initializes a new instance of the BasicTracks class.
         """
         self._main_track_points_list = []
 
-    def add_track_point(self, trk_point: BasicPoint):
+    def __iter__(self):
+        """
+        Returns an iterator for the list of track points.
+        This allows instances of this class to be used in a for loop.
+
+        :return: Iterator for the list of track points
+        :rtype: iter
+        """
+        return iter(self._main_track_points_list)
+
+    def add_track_point(self, trk_point: Union[BasicPoint, RawTrkPoint, AnalyzedTrkPoint]):
         self._main_track_points_list.append(trk_point)
 
     def get_track_point(self, i):
