06494555d949b22ece8fc196cacf044dd4adc622
PenHsuanWang <willy118116@gmail.com>
2024-04-05 14:56:17 +0800

    update document theme setting

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import renku_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -17,14 +18,14 @@ release = '0.0.0'
 
 extensions = ['sphinx.ext.autodoc']  # Add 'sphinx.ext.autodoc' to the list
 
-templates_path = ['_templates']
+# templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_theme = 'renku'
 html_theme_path = ['_themes']
-html_theme = 'renku_sphinx_theme'
 
 html_static_path = ['_static']
