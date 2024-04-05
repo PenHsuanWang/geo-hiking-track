e5627e0df599d221712694887360efced2792fc1
PenHsuanWang <willy118116@gmail.com>
2024-04-05 14:14:43 +0800

    update document theme setting

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import solar_theme
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -25,7 +24,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'solar_theme'
-html_theme = 'sphinx_rtd_theme'
-# html_theme_path = [solar_theme.theme_path]
+html_theme_path = ['_themes']
+html_theme = 'renku_sphinx_theme'
+
 html_static_path = ['_static']
