848373954eb088d6e121df33e17471da8dffa3cb
PenHsuanWang <willy118116@gmail.com>
2024-12-02 02:30:34 +0800

    add image point to map

--- src/visualizartion/map_drawer.py
+++ src/visualizartion/map_drawer.py
@@ -1,6 +1,8 @@
+# src/visualization/map_drawer.py
+
 import folium
 from src.geo_objects.geo_points.analyzed_geo_points import RestTrkPoint
-
+from src.geo_objects.geo_points.image_points import ImagePoint
 
 class FoliumMapDrawer:
     """
@@ -33,7 +35,7 @@ class FoliumMapDrawer:
         :param color: The color of the polyline, defaults to None
         :type color: str, optional
         """
-        self.fmap.add_child(folium.PolyLine(locations=point_list, weight=weight))
+        self.fmap.add_child(folium.PolyLine(locations=point_list, weight=weight, color=color))
 
     def add_tracks(self, input_tracks, **kwargs):
         """
@@ -70,43 +72,86 @@ class FoliumMapDrawer:
         if not isinstance(points, list):
             points = [points]
 
-        if not point_info == '':
+        if point_info != '':
             point_info += '<br>'
 
         for i in points:
             point_location = [i.lat, i.lon]
 
             if isinstance(i, RestTrkPoint):
+                # Existing logic for RestTrkPoint remains unchanged
                 popup_info = ('休息點' + '<br>' +
                               str(i.get_start_time().strftime('%H:%M')) + ' ~ ' + str(i.get_end_time().strftime('%H:%M')) +
                               '<br>' + 'Elev: ' + str(round(i.elev, 0)) + ' M')
-            else:
-                popup_info = point_info+str(i.time.strftime('%H:%M'))+'<br>'+i.get_note()+'<br>'+str(round(i.elev, 0)) + " M"
+                popup = folium.Popup(
+                    popup_info,
+                    max_width=150,
+                    min_width=70
+                )
+                if point_type == 'marker':
+                    self.fmap.add_child(
+                        folium.Marker(
+                            location=point_location,
+                            icon=folium.Icon(color=point_color, icon='info-sign', prefix='glyphicon'),
+                            popup=popup
+                        )
+                    )
+                else:
+                    self.fmap.add_child(
+                        folium.Circle(
+                            location=point_location,
+                            color=point_color,
+                            radius=point_radius,
+                            popup=popup,
+                            fill=True,
+                            fill_opacity=alpha
+                        )
+                    )
 
-            popup = folium.Popup(
-                popup_info,
-                max_width=150,
-                min_width=70
-            )
-            if point_type == 'marker':
+            elif isinstance(i, ImagePoint):
+                # New logic for ImagePoint to display images from URLs
+                popup_html = i.get_popup_info()
+                iframe = folium.IFrame(html=popup_html, width=300, height=300)
+                popup = folium.Popup(iframe, max_width=500)
+
+                # Use a custom icon for image points (e.g., a camera icon)
+                icon = folium.Icon(color=point_color, icon='camera', prefix='fa')  # Using Font Awesome icons
+
                 self.fmap.add_child(
                     folium.Marker(
                         location=point_location,
-                        icon=folium.Icon(color=point_color, icon='info-sign', prefix='glyphicon'),
+                        icon=icon,
                         popup=popup
                     )
                 )
+
             else:
-                self.fmap.add_child(
-                    folium.Circle(
-                        location=point_location,
-                        color=point_color,
-                        radius=point_radius,
-                        popup=popup,
-                        fill=True,
-                        fill_opacity=alpha
-                    )
+                # Existing logic for other points (e.g., waypoints) remains unchanged
+                popup_info = point_info + str(i.time.strftime('%H:%M')) + '<br>' + i.get_note() + '<br>' + str(round(i.elev, 0)) + " M"
+                popup = folium.Popup(
+                    popup_info,
+                    max_width=150,
+                    min_width=70
                 )
+                if point_type == 'marker':
+                    self.fmap.add_child(
+                        folium.Marker(
+                            location=point_location,
+                            icon=folium.Icon(color=point_color, icon='info-sign', prefix='glyphicon'),
+                            popup=popup
+                        )
+                    )
+                else:
+                    self.fmap.add_child(
+                        folium.Circle(
+                            location=point_location,
+                            color=point_color,
+                            radius=point_radius,
+                            popup=popup,
+                            fill=True,
+                            fill_opacity=alpha
+                        )
+                    )
 
     def save(self, out_file: str):
         """
@@ -115,7 +160,7 @@ class FoliumMapDrawer:
         :param out_file: The path of the file where the map will be saved
         :type out_file: str
         """
-        if not str(out_file).__contains__('.html'):
+        if not out_file.endswith('.html'):
             out_file += '.html'
         self.fmap.save(out_file)
-        print('saving map: {}'.format(out_file))
+        print('Saving map: {}'.format(out_file))
